6b12e469dbed7b9d6b26f55b1888dbb55fa62814
dbrudals <rudals20060@gmail.com>
2023-04-09 10:31:15 +0900

    modify

--- IhyeonP/src/main/ih-frontend/src/main.js
+++ IhyeonP/src/main/ih-frontend/src/main.js
@@ -2,8 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
-import "./assets/css/nucleo-icons.css";
-import "./assets/css/nucleo-svg.css";
+// import "./assets/css/nucleo-icons.css";
+// import "./assets/css/nucleo-svg.css";
 import MaterialDashboard from "./material-dashboard";
 
 
--- IhyeonP/src/main/ih-frontend/src/router/index.js
+++ IhyeonP/src/main/ih-frontend/src/router/index.js
@@ -1,16 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
-// import Tables from "../views/Tables.vue";
-import Flows from "../views/Flows.vue";
-import Mbti from "../views/Mbti.vue";
-import Vm from "../views/Vm.vue";
-import Error from "../views/Error.vue";
-//import Billing from "../views/Billing.vue";
-// import RTL from "../views/Rtl.vue";
-// import Notifications from "../views/Notifications.vue";
-// import Profile from "../views/Profile.vue";
-// import SignIn from "../views/SignIn.vue";
-// import SignUp from "../views/SignUp.vue";
+import LoginPage from "../views/LoginPage.vue";
 
 const routes = [
   {
@@ -24,60 +14,11 @@ const routes = [
     component: Dashboard,
   },
   {
-    path: "/flows",
-    name: "FLOWS",
-    component: Flows,
+    path: "/login",
+    name: "LoginPage",
+    component: LoginPage,
   },
-  {
-    path: "/mbti",
-    name: "Mbti",
-    component: Mbti,
-  },
-  {
-    path: "/vm",
-    name: "Vm",
-    component: Vm,
-  },
-  {
-    path: "/error",
-    name: "Error",
-    component: Error,
-  },
-  // {
-  //   path: "/tables",
-  //   name: "Tables",
-  //   component: Tables,
-  // },
-  // {
-  //   path: "/billing",
-  //   name: "Billing",
-  //   component: Billing,
-  // },
-  // {
-  //   path: "/rtl-page",
-  //   name: "RTL",
-  //   component: RTL,
-  // },
-  // {
-  //   path: "/notifications",
-  //   name: "Notifications",
-  //   component: Notifications,
-  // },
-  // {
-  //   path: "/profile",
-  //   name: "Profile",
-  //   component: Profile,
-  // },
-  // {
-  //   path: "/sign-in",
-  //   name: "SignIn",
-  //   component: SignIn,
-  // },
-  // {
-  //   path: "/sign-up",
-  //   name: "SignUp",
-  //   component: SignUp,
-  // },
+
 ];
 
 const router = createRouter({
