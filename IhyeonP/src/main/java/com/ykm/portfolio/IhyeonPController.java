232956526a6d10441829be983c05141678acd113
flycat-0329 <55021019+flycat20050329@users.noreply.github.com>
2023-05-14 12:35:13 +0900

    OnePageFrontend
    2023-05-14 12:34
    OnePage 예제 적용
    CORS 해결 및 axios 통신 확인

--- IhyeonP/src/main/java/com/ykm/portfolio/IhyeonPController.java
+++ IhyeonP/src/main/java/com/ykm/portfolio/IhyeonPController.java
@@ -1,5 +1,6 @@
 package com.ykm.portfolio;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class IhyeonPController {
 
-	@RequestMapping(value = "/")
-	public String home() {
-		return "";
+//	@RequestMapping(value = "/")
+//	public String home() {
+//		return "";
+//	}
+	
+	@GetMapping("/a")
+	public String axiosTest(){
+		return "axios 연결됨";
 	}
 }
