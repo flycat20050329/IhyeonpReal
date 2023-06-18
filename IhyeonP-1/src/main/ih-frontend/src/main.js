5816effb82c30706a0b619c09aa7fce600ae75ca
flycat-0329 <55021019+flycat20050329@users.noreply.github.com>
2023-06-18 11:07:08 +0900

    yes fullpage
    풀페이지 + 점심 코드

--- IhyeonP-1/src/main/ih-frontend/src/main.js
+++ IhyeonP-1/src/main/ih-frontend/src/main.js
@@ -5,9 +5,17 @@ import store from "./store";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from './plugins/font-awesome'
+import axios from 'axios'
+
+import 'vue-fullpage.js/dist/style.css'
+import VueFullPage from 'vue-fullpage.js'
 
 createApp(App)
   .use(router)
   .use(store)
+  .use(VueFullPage)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
+
+App.config.globalProperties.$axios = axios
+axios.defaults.baseURL = "http://localhost:8080/";
